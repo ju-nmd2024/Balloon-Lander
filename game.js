64e1b7fd440f5bc137198d4c6ac93f92bba3a6f0
Sehr06 <lose24wo@student.ju.se>
2024-11-15 16:56:08 +0100

    functioning screens

--- game.js
+++ game.js
@@ -1,9 +1,14 @@
-let x = 450;
-let y = 600;
-let s = 0.5;
+function setup() {
+  createCanvas(600, 500);
+  background(167, 199, 207);
+}
 
-createCanvas(600, 600);
-background(167, 199, 207);
+let balloonX = 450;
+let balloonY = 600;
+let s = 0.5;
+let state = "start";
+let velocity = 0.1;
+let acceleration = 0;
 
 function balloon(x, y, s) {
   //BALLOON
@@ -56,19 +61,58 @@ function balloon(x, y, s) {
   endShape();
 }
 
-function draw() {
-  //CHARACTER MOVE UP AND DOWW
+function startScreen() {
+  //BACKGROUND COLOR
+  background(70, 130, 180);
+  //BUTTON
+  fill(0, 0, 0);
+  rect(200, 200, 200, 70, 10);
+  //TEXT
+  fill(255, 255, 255);
+  noStroke();
+  textAlign(CENTER);
+  text("START", 300, 250);
+  textSize(40);
+  //CLOUDS
+  ellipse(130, 120, 150, 90, 20);
+}
+
+function gameScreen() {
   background(167, 199, 207);
-  balloon(x, y, s);
+  text("game", 300, 250);
+  balloon(balloonX, balloonY);
 
-  if (y >= 100) {
-    y = y - 5;
-  }
+  //CHARACTER MOVE UP AND DOWN
+  background(167, 199, 207);
+  balloon(balloonX, balloonY, s);
 
-  /*
- y = y + speed;
-  if (y > 300 || y < 100) {
-    speed = speed * -1;
+  if (balloonY >= 100) {
+    balloonY = balloonY - 5;
   }
-  */
+}
+
+function resultScreen() {
+  background(255);
+  text("result", 300, 250);
+}
+
+state = "start";
+function mouseClicked() {
+  if (state === "start") {
+    state = "game";
+  } else if (state === "game") {
+    state = "result";
+  } else if (state === "result") {
+    state = "game";
+  }
+}
+
+function draw() {
+  if (state === "start") {
+    startScreen();
+  } else if (state === "game") {
+    gameScreen();
+  } else if (state === "result") {
+    resultScreen();
+  }
 }
